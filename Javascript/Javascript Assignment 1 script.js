2d3e9a8833968b670d87c03291e37739b9dc014b
Ebonee Singleteary <Ebonee@Ebonees-MacBook-Pro.local>
2024-02-17 12:31:06 -0500

    Update Feb 17

---
+++ Javascript/Javascript Assignment 1 script.js
@@ -0,0 +1,13 @@
+console.log("Hello World")
+
+document.write('<br>')
+document.write('<br>')
+
+console.log("Ebonee's favorite color is blue")
+
+document.write('<br>')
+document.write('<br>')
+
+console.log("10 + 11 = 21")
+
+console.log('Ebonee' + 'Singleteary')
